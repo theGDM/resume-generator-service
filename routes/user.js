52f837b1c2cf7a3dc44a9cb902bbdcb3031ab6f1
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2024-08-06 01:10:14 +0530

    Gyandeep | Added routes for the users

---
+++ routes/user.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { deleteUser, getAllUsers, getUser, updateUser } from '../controllers/userController.js';
+import { verifyUser } from '../utils/verifyToken.js';
+
+const router = express.Router();
+
+//UPDATE
+router.put("/:id", verifyUser, updateUser); //so the owner or admin can update the user
+
+//DELETE
+router.delete("/:id", verifyUser, deleteUser);
+
+//GET
+router.get("/:emailId", getUser);
+
+//GET ALL
+router.get("/", getAllUsers); //only admin can get all the data, not every user
+
+export default router;
