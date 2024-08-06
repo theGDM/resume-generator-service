6b8bc546ea5a181e70a037181100d3a1685b4786
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2024-08-06 22:03:43 +0530

    Gyandeep | Updated routes for resume

---
+++ routes/resume.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import { createResume, deleteResume, getAllResumes, getAllResumesForUser, getResume, updateResume } from '../controllers/resumeController';
+
+const router = express.Router();
+
+//Get All the resumes for user
+router.get("/:userId/resume", getAllResumesForUser);
+
+//CREATE
+router.post("/:userId", createResume);
+
+//UPDATE
+router.put("/:resumeId", updateResume);
+
+//DELETE
+router.delete("/:resumeId", deleteResume);
+
+//GET
+router.get("/:resumeId", getResume);
+
+//GET ALL
+router.get("/", getAllResumes);
+
+export default router;
